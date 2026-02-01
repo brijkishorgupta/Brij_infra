07854e3d26e508ec22300953340427b2554b989e
brijkishorgupta <brijkishorgupta290@gmail.com>
2026-02-01 21:16:23 +0530

    add

--- vm.tf
+++ vm.tf
@@ -3,10 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = "central india"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "brij-rg"
-  location = "central india"
-}
+
 #implicit dependency
 resource "azurerm_virtual_network" "vnet" {
   name                = "depends-vnet"
